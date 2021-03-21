053c5331a88067fc4d7b390a8ae9a7e938c1606c
Jack Huntington-Rainey <jack@jacks-mbp-2.myfiosgateway.com>
2021-03-21 17:04:20 -0400

    Layout model, controller, and routes

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,10 +2,46 @@ const Flight = require('../models/flight');
 
 module.exports = {
   index,
+  show,
+  create,
+  update,
+  delete: deleteFlight
 };
 
 function index(req, res) {
   Flight.find({}, function(err, flights) {
+    if (err) console.log(err);
     res.render('flights/index', { flights });
   });
+}
+
+function show(req, res) {
+  Flight.findById(req.params.id, function(err, flight) {
+    if (err) console.log(err);
+    res.send(flight);
+  })
+}
+
+function create(req, res) {
+  Flight.create(req.body, function(err, flight) {
+    if (err) console.log(err);
+    res.redirect(`/flights/${flight._id}`);
+  })
+}
+
+function update(req, res) {
+  Flight.findById(req.params.id, function(err, flight) {
+    const updatedFlight = {...flight, ...req.body};
+    Flight.findByIdAndUpdate(req.params.id, updatedFlight, function(err, pupper) {
+      if (err) console.log(err);
+      res.redirect(`/flights/${req.params.id}`);
+    })
+  })
+}
+
+function deleteFlight(req, res) {
+  Flight.findOneAndDelete(req.params.id, function(err, deletedFlight) {
+    if (err) console.log(err);
+    res.redirect('/flights');
+  })
 }
--- routes/flights.js
+++ routes/flights.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 const flightsCtrl = require('../controllers/flights.js');
 
-// GET /movies
+// GET /flights
 router.get('/', flightsCtrl.index);
+// GET /flights/:id
+router.get('/:id', flightsCtrl.show);
+// POST /flights
+router.post('/', flightsCtrl.create);
+// PUT /flights/:id
+router.put('/:id', flightsCtrl.update);
+// DELETE /flights:id
+router.delete('/:id', flightsCtrl.delete);
 
 module.exports = router;
